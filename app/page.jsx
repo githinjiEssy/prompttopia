b554cba6e1e5f76d5998225361c5704b6d83b139
githinjiEssy <esthergithinji49@gmail.com>
2024-12-07 12:08:27 +0300

    home

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          Disover & Share
+          <br className="max-md:hidden"/>
+          <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        </h1>
+      </section>
+    </div>
+  )
+}
+
+export default Home
